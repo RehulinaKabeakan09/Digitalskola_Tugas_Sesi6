71923c71c6a838b65014085924fd705842095cd0
Rehulina Kabeakan <rehulinakabeakan@gmail.com>
2026-02-10 17:10:29 +0700

    (added DetailTugasSesi6.js)

---
+++ DetailTugasSesi6.js
@@ -0,0 +1,10 @@
+"Tugas Sesi 6 - API Testing"
+
+1. Buatlah collection di postman dengan format “Nama – Batch 14”
+2. Buatlah sample request API dari url https://belajar-bareng.onrender.com/
+        a. Buat 1 sample request GET (Get Users)
+        b. Buat 1 sample request POST (Login)
+3. Simpan token(response dari Login) ke variabel collection
+4. Gunakan token tersebut di Header untuk request selanjutnya(GET Users)
+5. Tambahkan 2 assert di setiap request di atas(status code dan response body)
+6. Export collection beserta variabel tersebut(.json) dan upload ke dalam LMS yang sudah di tentukan
